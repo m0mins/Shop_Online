80fd71aac4cddd134607d92d9f1c212a0bea9b61
m0mins <momin.pstu.cse11.bd@gmail.com>
2023-12-30 00:05:58 +0600

    product see by category based

--- App_Home/views.py
+++ App_Home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from App_Home.models import Slider
 # Create your views here.
 def home(request):
-    return render(request,'App_Home/home.html')
+    slider=Slider.objects.all()
+    return render(request,'App_Home/home.html',context={'slider':slider})
--- App_Products/urls.py
+++ App_Products/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from App_Products import views
+from .views import SubCategoryProductListView
 
 app_name='App_Products'
 
 urlpatterns = [
- 
+    path('cate/',views.category,name='cate'),
+    path('sub/<str:category>/',views.sub_men_cloth,name='sub'),
+    path('sub_category/<int:sub_category_id>/', SubCategoryProductListView.as_view(), name='sub_category_products'),
+
 ]
