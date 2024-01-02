a72aee21c06b9b2d48c50549000b33b2f81f5102
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-01-02 14:35:50 +0600

    Done product details

--- App_Products/views.py
+++ App_Products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponseRedirect, redirect,get_object_or_404
+from django.shortcuts import render, HttpResponseRedirect, redirect,get_object_or_404,Http404
 from django.views.generic import ListView, DetailView
 
 from App_Products.models import Product,Product_Size,Category,Sub_Category
@@ -8,17 +8,15 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def all_products(request):
-    try:
 
         products = Product.objects.all()
-
         product_details = []
 
         for product in products:
             size_variants = product.size_variants.all()
             color_variants = product.color_variants.all()
-
             product_detail = {
+                'products':products,
                 'name': product.name,
                 'sku':product.sku,
                 'price':product.price,
@@ -32,13 +30,12 @@ def all_products(request):
             }
 
             product_details.append(product_detail)
+        combined_data = [{'object': obj, 'product_name': name} for obj, name in zip(products, product_details)]
 
-        context = {'product_details': product_details}
+        context = {'combined_data': combined_data}
 
         return render(request, 'App_Products/Category/all_products.html', context)
-    except Category.DoesNotExist:
-                
-                return render(request, 'App_Products/Category/category_not_found.html')
+
 
 
 def category_products(request, category_name, subcategory_name=None):
@@ -52,10 +49,10 @@ def category_products(request, category_name, subcategory_name=None):
         subcategory_products = selected_subcategory.sub_category.all()
 
     products = Product.objects.filter(category=category)
-    
     subcategories = Sub_Category.objects.filter(categorys=category)
 
     context = {
+         
         'category': category,
         'products': subcategory_products if subcategory_name else products,
         'subcategories': subcategories if not subcategory_name else [],
@@ -64,4 +61,27 @@ def category_products(request, category_name, subcategory_name=None):
         'subcategory':subcategory_name
     }
 
-    return render(request, 'App_Products/Sub_Category/sub_products.html', context)
+    return render(request, 'App_Products/Sub_Category/category_products.html', context)
+
+def single_product(request, pk):
+    
+    product = Product.objects.get(id=pk)
+    
+    size_variants = product.size_variants.all()
+    color_variants = product.color_variants.all()
+
+    context = {
+        'name': product.name,
+        'price': product.price,
+        'old_price': product.old_price,
+        'preview': product.preview_text,
+        'description': product.detail_text,
+        'additional_info': product.additional_info,
+        'sizes': [size_variant.size.name for size_variant in size_variants],
+        'colors': [color_variant.color.name for color_variant in color_variants],
+        'sku': product.sku,
+        'category': product.category.title,
+        'mainimage': product.mainimage
+    }
+
+    return render(request, 'App_Products/single_product.html', context)
