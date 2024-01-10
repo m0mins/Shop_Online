fc00336a53e5b569da4722a8dcf322ae2efd16cb
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-01-10 18:25:50 +0600

    yml changed

--- ShopOnline/settings.py
+++ ShopOnline/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-@+oiguv@le3@u#-3a!tj559s!)v3xyq*)yb0!thf-t*$($v7ea
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
