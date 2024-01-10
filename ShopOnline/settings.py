71a3ec15d1e8f59715aff96c9dcf4a5d95d339bc
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-01-10 19:15:41 +0600

    updated settings file with base url

--- ShopOnline/settings.py
+++ ShopOnline/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/' 
 LOGIN_URL='/accounts/login/'
-
+BASE_URL = 'http://18.141.173.32/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
