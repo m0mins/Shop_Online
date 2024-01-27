9881d15d88363e8a76bb19f2324d404e0e458544
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-01-27 08:48:44 +0600

    sidebar product details done

--- App_Order/templatetags/cart_tags.py
+++ App_Order/templatetags/cart_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from App_Order.models import Order
+from App_Order.models import Order,Cart
 from App_Products.models import Product,Size,Product_Size,Product_Color
 from django.db.models import Count
 
@@ -13,6 +13,27 @@ def cart_total(user):
         return order[0].orderitems.count()
     else:
         return 0
+
+@register.filter
+def sub_total(user):
+    carts = Cart.objects.filter(user=user, purchased=False)
+    cart_items = []
+    total_price = 0.0
+
+    for cart_item in carts:
+        total_price += float(cart_item.get_total())
+        cart_items.append(cart_item)
+
+    return {'cart_items': cart_items, 'total_price': total_price}
+    #total_price = 0.0
+    #total_quantity=0
+    #carts = Cart.objects.filter(user=user,purchased=False)
+
+    #for cart_item in carts:
+        #total_price += float(cart_item.get_total())
+        #total_quantity += cart_item.quantity
+
+    #return {'total_price': total_price, 'total_quantity': total_quantity,'carts':carts}
 
 @register.filter
 def get_product_count(category):
@@ -37,3 +58,4 @@ def product_count_by_color(color):
         return color_counts
     else:
         return 0
+    
