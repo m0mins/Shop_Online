51d624572ea695176a94ffa86e68d34112932162
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-02-24 16:46:00 +0600

    updated again BillingAddress in App_Payment's admin

--- App_Payment/admin.py
+++ App_Payment/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from App_Payment.models import BillingAddress
 # Register your models here.
 
-admin.site.register(BillingAddress)
-
 class AdminBillingAddress(admin.ModelAdmin):
     list_display=['address','zipcode','city','country']
 admin.site.register(BillingAddress,AdminBillingAddress)
+
+
