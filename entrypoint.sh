cc00a15141d2d634f4e7470893be18137d9ac8e4
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-02-24 12:12:06 +0600

    added create super admin

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,9 @@ set -e
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
+# Create a superuser if it doesn't exist
+if [ -z "$(python3 manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='admin').exists())")" ]; then
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@gmail.com', '1')" | python3 manage.py shell
+fi
+
 exec "$@"
