9be1ed2ed8e41104946c8efc394a6547d9fbf67e
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-02-25 17:14:19 +0600

    updated again3 Billing address model

--- App_Payment/models.py
+++ App_Payment/models.py
@@ -7,7 +7,7 @@ class BillingAddress(models.Model):
     address = models.CharField(max_length=264, blank=True)
     zipcode = models.CharField(max_length=10, blank=True)
     city = models.CharField(max_length=30, blank=True)
-    country = models.CharField(max_length=20, blank=True)
+    #country = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
         return f'{self.user.profile.username} billing address'
