482f129893ab833df415282df59ba629282eead3
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-02-25 17:16:12 +0600

    updated again4 Billing address model

--- App_Payment/models.py
+++ App_Payment/models.py
@@ -4,9 +4,9 @@ from django.conf import settings
 
 class BillingAddress(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    address = models.CharField(max_length=264, blank=True)
-    zipcode = models.CharField(max_length=10, blank=True)
-    city = models.CharField(max_length=30, blank=True)
+    #address = models.CharField(max_length=264, blank=True)
+    #zipcode = models.CharField(max_length=10, blank=True)
+    #city = models.CharField(max_length=30, blank=True)
     #country = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
