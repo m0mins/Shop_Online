295473d5c72dc94e9d9451066b622ed3362ea25a
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-02-25 17:01:55 +0600

    updated Billing address model

--- App_Payment/admin.py
+++ App_Payment/admin.py
@@ -3,7 +3,7 @@ from App_Payment.models import BillingAddress
 # Register your models here.
 
 class AdminBillingAddress(admin.ModelAdmin):
-    list_display=['user','additional_info','compnay_name']
+    list_display=['user']
 admin.site.register(BillingAddress,AdminBillingAddress)
 
 
