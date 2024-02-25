567eecba833bc8b52418d002f48592ca4ea1354c
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-02-25 14:51:28 +0600

    issue 6 in checkuot page

--- App_Payment/views.py
+++ App_Payment/views.py
@@ -12,24 +12,33 @@ from django.contrib import messages
 
 @login_required  
 def checkout(request):
-    order=Order.objects.filter(user=request.user,ordered=False)
-    total_orders=0
-    if order.exists():
-        total_orders +=order[0].orderitems.count()
-        order=order[0]
-    carts=Cart.objects.filter(user=request.user,purchased=False)
-    cart_items=[]
-    total_price=0.0
-    for card_item in carts:
-        total_price +=float(card_item.get_total())
-        cart_items.append(card_item)
-
-    context={
-        'order':'order',
-        'cart_items':'cart_items',
-        'total_orders':'total_orders',
-        'total_price':'total_price',
-        'carts':'carts'
-    }
+    #order=Order.objects.filter(user=request.user,ordered=False)
+    #total_orders=0
+    #if order.exists():
+    #    total_orders +=order[0].orderitems.count()
+    #    order=order[0]
+    #carts=Cart.objects.filter(user=request.user,purchased=False)
+    #cart_items=[]
+    #total_price=0.0
+    #for card_item in carts:
+    #    total_price +=float(card_item.get_total())
+    #    cart_items.append(card_item)
+#
+    #context={
+    #    'order':'order',
+    #    'cart_items':'cart_items',
+    #    'total_orders':'total_orders',
+    #    'total_price':'total_price',
+    #    'carts':'carts'
+#
+    carts = Cart.objects.filter(user=request.user, purchased=False)
+    orders = Order.objects.filter(user=request.user, ordered=False)
+    if carts.exists() and orders.exists():
+        order = orders[0]
+        return render(request, 'App_Payment/checkout.html', context={'carts':carts, 'order':order})
+    else:
+        messages.warning(request, "You don't have any item in your cart!")
+        return redirect("App_Home:home")
     
-    return render( request,'App_Payment/checkout.html',context)
+    
+   
