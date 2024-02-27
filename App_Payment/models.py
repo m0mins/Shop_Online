91f6cd09e46b85796d5b0814e9a34606e7f3df57
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-02-27 15:38:25 +0600

    added billings fields

--- App_Payment/models.py
+++ App_Payment/models.py
@@ -5,14 +5,17 @@ from App_Accounts.models import User
 
 class BillingAddress(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    #user = models.ForeignKey(User, on_delete=models.CASCADE)
-    #company_name=models.CharField(max_length=264, blank=True)
-    #additional_info = models.TextField(max_length=264, blank=True)
+    firstName=models.CharField(max_length=100 ,blank=True,null=True)
+    lastName=models.CharField(max_length=100 ,blank=True,null=True)
+    country = models.CharField(max_length=20, blank=True)
+    city = models.CharField(max_length=30, blank=True)
+    postalCode = models.CharField(max_length=10, blank=True)
+    company_name=models.CharField(max_length=264, blank=True)
+    email=models.CharField(max_length=100 ,blank=True,null=True)
+    phone=models.CharField(max_length=100 ,blank=True,null=True)
+    address = models.CharField(max_length=264, blank=True)
+    additional_info = models.TextField(max_length=264, blank=True)
     
-    #address = models.CharField(max_length=264, blank=True)
-    #zipcode = models.CharField(max_length=10, blank=True)
-    #city = models.CharField(max_length=30, blank=True)
-    #country = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
         return f'{self.user.profile.username} billing address'
