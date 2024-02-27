a9881e5df68b53cd5bd4818f6f3e78df8b2701f2
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-02-27 17:25:44 +0600

    added csrf token

--- App_Payment/views.py
+++ App_Payment/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 # Model
 from App_Order.models import Cart, Order
 from App_Products.models import Product
+from App_Payment.models import BillingAddress
 # Messages
 from django.contrib import messages
 # Create your views here.
@@ -25,10 +26,21 @@ def checkout(request):
         phone=request.POST.get('phone')
         address=request.POST.get('address')
         additional_info=request.POST.get('additional_info')
-    if carts.exists() and orders.exists():
-        order = orders[0]
+        saved_address, created = BillingAddress.objects.get_or_create(user=request.user)
+        saved_address.firstName=firstName
+        saved_address.lastName=lastName
+        saved_address.country=country
+        saved_address.city=city
+        saved_address.postalCode=postalCode
+        saved_address.company_name=company
+        saved_address.email=email
+        saved_address.phone=phone
+        saved_address.address=address
+        saved_address.additional_info=additional_info
+        saved_address.save
+        if carts.exists() and orders.exists():
+            order = orders[0]
     
-
         return render(request, 'App_Payment/checkout.html', context={'carts':carts, 'order':order})
     else:
         messages.warning(request, "You don't have any item in your cart!")
