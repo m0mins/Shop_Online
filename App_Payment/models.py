73cd9e612998df24801f693e88a6eeea1e23f890
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-02-29 19:37:11 +0600

    updated BillingAddr

--- App_Payment/models.py
+++ App_Payment/models.py
@@ -47,18 +47,18 @@ class BillingAddr(models.Model):
     additional_info = models.TextField(max_length=264, blank=True)
     
 
-    def __str__(self):
-        return f'{self.user.profile.username} billing address'
-
-    def is_fully_filled(self):
-        field_names = [f.name for f in self._meta.get_fields()]
-
-        for field_name in field_names:
-            value = getattr(self, field_name)
-            if value is None or value=='':
-                return False
-        return True
-
-
-    class Meta:
-        verbose_name_plural = "Billing Addresses"
+    #def __str__(self):
+    #    return f'{self.user.profile.username} billing address'
+#
+    #def is_fully_filled(self):
+    #    field_names = [f.name for f in self._meta.get_fields()]
+#
+    #    for field_name in field_names:
+    #        value = getattr(self, field_name)
+    #        if value is None or value=='':
+    #            return False
+    #    return True
+#
+#
+    #class Meta:
+    #    verbose_name_plural = "Billing Addresses"
