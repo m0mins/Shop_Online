073b4df26b296ed6f2664f48214f9c2f76652a79
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-02-29 19:26:21 +0600

    added BillingAddr in admin

--- App_Payment/admin.py
+++ App_Payment/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from App_Payment.models import BillingAddress
+from App_Payment.models import BillingAddress,BillingAddr
 # Register your models here.
 
 admin.site.register(BillingAddress)
+admin.site.register(BillingAddr)
 #class AdminBillingAddress(admin.ModelAdmin):
 #    list_display=['user','company_name','additional_info']
 #admin.site.register(BillingAddress,AdminBillingAddress)
