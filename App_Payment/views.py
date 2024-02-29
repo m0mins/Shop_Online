93b70ac36a1ae49f9d5787f3e69e0345b51a2f84
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-02-29 11:48:40 +0600

    comments checkout login code

--- App_Payment/views.py
+++ App_Payment/views.py
@@ -17,29 +17,29 @@ def checkout(request):
     #orders = Order.objects.filter(user=request.user, ordered=False)
     if request.method=='POST':
         firstName=request.POST.get('firstName')
-        lastName=request.POST.get('lastName')
-        country=request.POST.get('country')
-        city=request.POST.get('city')
-        postalCode=request.POST.get('postalCode')
-        company=request.POST.get('company')
-        email=request.POST.get('email')
-        phone=request.POST.get('phone')
-        address=request.POST.get('address')
-        additional_info=request.POST.get('additional_info')
-        saved_address, created = BillingAddress.objects.get_or_create(user=request.user)
-        saved_address.firstName=firstName
-        saved_address.lastName=lastName
-        saved_address.country=country
-        saved_address.city=city
-        saved_address.postalCode=postalCode
-        saved_address.company_name=company
-        saved_address.email=email
-        saved_address.phone=phone
-        saved_address.address=address
-        saved_address.additional_info=additional_info
-        if saved_address.is_valid():
-            saved_address.save() 
-            messages.warning(request, "Updated your Billing Address")
+        #lastName=request.POST.get('lastName')
+        #country=request.POST.get('country')
+        #city=request.POST.get('city')
+        #postalCode=request.POST.get('postalCode')
+        #company=request.POST.get('company')
+        #email=request.POST.get('email')
+        #phone=request.POST.get('phone')
+        #address=request.POST.get('address')
+        #additional_info=request.POST.get('additional_info')
+        #saved_address, created = BillingAddress.objects.get_or_create(user=request.user)
+        #saved_address.firstName=firstName
+        #saved_address.lastName=lastName
+        #saved_address.country=country
+        #saved_address.city=city
+        #saved_address.postalCode=postalCode
+        #saved_address.company_name=company
+        #saved_address.email=email
+        #saved_address.phone=phone
+        #saved_address.address=address
+        #saved_address.additional_info=additional_info
+        #if saved_address.is_valid():
+        #    saved_address.save() 
+        #    messages.warning(request, "Updated your Billing Address")
     carts = Cart.objects.filter(user=request.user, purchased=False)              
     order_qs = Order.objects.filter(user=request.user, ordered=False)
     order_items = order_qs[0].orderitems.all()
