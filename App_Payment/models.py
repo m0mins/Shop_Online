6f451b93c513092a176e3bbd7be07e321d2d7c1b
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-02-29 19:16:03 +0600

    comment all the  fields of billing address

--- App_Payment/models.py
+++ App_Payment/models.py
@@ -5,30 +5,30 @@ from App_Accounts.models import User
 
 class BillingAddress(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    firstName=models.CharField(max_length=100 ,blank=True,null=True)
-    lastName=models.CharField(max_length=100 ,blank=True,null=True)
-    country = models.CharField(max_length=20, blank=True)
-    city = models.CharField(max_length=30, blank=True)
-    postalCode = models.CharField(max_length=10, blank=True)
-    company_name=models.CharField(max_length=264, blank=True)
-    email=models.CharField(max_length=100 ,blank=True,null=True)
-    phone=models.CharField(max_length=100 ,blank=True,null=True)
-    address = models.CharField(max_length=264, blank=True)
-    additional_info = models.TextField(max_length=264, blank=True)
-    
-
-    def __str__(self):
-        return f'{self.user.profile.username} billing address'
-
-    def is_fully_filled(self):
-        field_names = [f.name for f in self._meta.get_fields()]
-
-        for field_name in field_names:
-            value = getattr(self, field_name)
-            if value is None or value=='':
-                return False
-        return True
-
-
-    class Meta:
-        verbose_name_plural = "Billing Addresses"
+    #firstName=models.CharField(max_length=100 ,blank=True,null=True)
+    #lastName=models.CharField(max_length=100 ,blank=True,null=True)
+    #country = models.CharField(max_length=20, blank=True)
+    #city = models.CharField(max_length=30, blank=True)
+    #postalCode = models.CharField(max_length=10, blank=True)
+    #company_name=models.CharField(max_length=264, blank=True)
+    #email=models.CharField(max_length=100 ,blank=True,null=True)
+    #phone=models.CharField(max_length=100 ,blank=True,null=True)
+    #address = models.CharField(max_length=264, blank=True)
+    #additional_info = models.TextField(max_length=264, blank=True)
+    #
+#
+    #def __str__(self):
+    #    return f'{self.user.profile.username} billing address'
+#
+    #def is_fully_filled(self):
+    #    field_names = [f.name for f in self._meta.get_fields()]
+#
+    #    for field_name in field_names:
+    #        value = getattr(self, field_name)
+    #        if value is None or value=='':
+    #            return False
+    #    return True
+#
+#
+    #class Meta:
+    #    verbose_name_plural = "Billing Addresses"
