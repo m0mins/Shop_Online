f30c168e1f66232943661aa0b4f5fa9a60a57e7c
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-02-29 11:53:32 +0600

    Deleted Billing Address Model

--- App_Payment/admin.py
+++ App_Payment/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from App_Payment.models import BillingAddress
+#from App_Payment.models import BillingAddress
 # Register your models here.
 
-admin.site.register(BillingAddress)
+#admin.site.register(BillingAddress)
 #class AdminBillingAddress(admin.ModelAdmin):
 #    list_display=['user','company_name','additional_info']
 #admin.site.register(BillingAddress,AdminBillingAddress)
